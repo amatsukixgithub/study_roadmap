c6fb682785672f3d15f4ec22e5ceb34f02c90802
amatsukixprog <amatsukixprog@gmail.com>
2019-09-06 23:30:23 +0900

    routes.rbでstatic_pagesのアクションを指定する

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/contact'
-  get 'static_pages/about'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/home', to: 'static_pages#home'
+  get '/contact', to: 'static_pages#contact'
+  get '/about', to: 'static_pages#about'
 end
