b5d16f54bda306809cb653b9147404bf3d80b5da
amatsukixprog <amatsukixprog@gmail.com>
2019-09-09 15:56:36 +0900

    Add #14 管理者ユーザー機能

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_03_164730) do
+ActiveRecord::Schema.define(version: 2019_09_09_065253) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2019_09_03_164730) do
     t.string "comment"
     t.string "icon_pass"
     t.string "web_page"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
