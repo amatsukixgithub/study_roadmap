39a7214f576d37b09eb2c025b2c841c6c92586ac
amatsukixprog <amatsukixprog@gmail.com>
2019-09-09 15:03:19 +0900

    Add#2 ユーザー一覧からユーザーを削除する機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,15 @@ class UsersController < ApplicationController
   # devise のヘルパー ログイン済みユーザーのみアクセス許可
   before_action :authenticate_user!, only: [:index]
 
+  # ユーザー一覧ページ
   def index
     @users = User.all
+  end
+
+  # ユーザー一覧ページからのユーザー削除
+  def delete_user
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_path
   end
 end
