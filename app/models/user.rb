bf027ba0281ca91595aa17247d0eff0bd3560d95
amatsukixprog <amatsukixprog@gmail.com>
2019-12-16 21:26:10 +0900

    インデント修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,11 @@ class User < ApplicationRecord
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :validatable
 
   validates :name, presence: true, length: { in: 4..20 }
   validates :email, length: { maximum: 254 }
