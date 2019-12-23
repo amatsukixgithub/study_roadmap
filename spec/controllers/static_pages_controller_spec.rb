1b4e32d8d5cecce4fed7ed37257d8561d0fb4679
amatsukixprog <amatsukixprog@gmail.com>
2019-12-24 03:06:20 +0900

    static_pages_controllerのspecファイルを修正

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -12,4 +12,28 @@ RSpec.describe StaticPagesController, type: :controller do
       expect(response).to have_http_status "200"
     end
   end
+
+  describe '#about' do
+    it "正常なレスポンスを返す" do
+      get :about
+      expect(response).to be_successful
+    end
+
+    it "200レスポンスを返す" do
+      get :about
+      expect(response).to have_http_status "200"
+    end
+  end
+
+  describe '#contact' do
+    it "正常なレスポンスを返す" do
+      get :contact
+      expect(response).to be_successful
+    end
+
+    it "200レスポンスを返す" do
+      get :contact
+      expect(response).to have_http_status "200"
+    end
+  end
 end
